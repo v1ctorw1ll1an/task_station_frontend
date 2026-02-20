9ab75efd6b0c71712bd96cc23c52c89b41486eb4
V1ctorW1ll1an <victorwillian1012@gmail.com>
2026-02-20 15:21:49 -0300

    Redirect to first-access if password reset required

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,5 +8,9 @@ export default async function DashboardLayout({ children }: { children: React.Re
     redirect('/login');
   }
 
+  if (session.user.mustResetPassword) {
+    redirect('/first-access');
+  }
+
   return <>{children}</>;
 }
